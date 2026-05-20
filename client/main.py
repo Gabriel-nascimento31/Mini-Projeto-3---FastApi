87c8142bf2578c11f89aeb93a8be4eca3193cc12
brendolalves <brendol.alves@gmail.com>
2026-05-20 15:23:22 -0300

    atualização do cliente
    foi adicionado o direcionamento para o servidor de compras

--- client/main.py
+++ client/main.py
@@ -1,12 +1,21 @@
 import requests
 
-URL = "http://127.0.0.1:8000/produtos"
+#URL = "http://127.0.0.1:8000/produtos"
 
-resposta = requests.get(URL)
+URL_PRODUTOS = "http://127.0.0.1:8000/produtos"
+URL_COMPRAR = "http://127.0.0.1:8000/comprar"
 
-if resposta.status_code == 200:
-    dados_produtos = resposta.json()
+print("--Produtos Disponíveis--")
+resposta_produtos = requests.get(URL_PRODUTOS)
+
+if resposta_produtos.status_code == 200:
+    dados_produtos = resposta_produtos.json()
     for produto in dados_produtos:
-        print(f"Produto: {produto['nome']} - R$ {['preco']}")
+        status_estoque = "Em estoque" if produto["em_estoque"] else "Sem estoque"
+        print(f"ID: {produto['id']} | {produto["nome"]} - R$ {['preco']}({status_estoque})")
 else:
-    print("Erro ao consumir o API. ")
+    print("Erro ao listar os produtos da API.")
+    exit()
+print("\n" + "="*50 + "\n")
+
+
